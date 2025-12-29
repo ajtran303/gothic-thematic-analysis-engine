b58ae1aa8c136c96aeb658b54c0835860f588b1b
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2025-12-29 11:24:05 -0500

    prep train_generator script for use on my computer with a gpu

--- scripts/generate.py
+++ scripts/generate.py
@@ -14,9 +14,11 @@ def generate(themes, max_length=200):
         inputs.input_ids,
         max_length=max_length,
         num_return_sequences=1,
-        temperature=0.8,
-        top_p=0.9,
+        temperature=0.7,
+        top_p=0.92,
         do_sample=True,
+        repetition_penalty=1.2,
+        no_repeat_ngram_size=3,
         pad_token_id=tokenizer.eos_token_id
     )
     
--- scripts/train_generator.py
+++ scripts/train_generator.py
@@ -8,7 +8,7 @@ from transformers import (
 from datasets import load_dataset
 
 
-model_name = "EleutherAI/pythia-410m"
+model_name = "EleutherAI/pythia-160m"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
 
@@ -23,7 +23,7 @@ def tokenize(examples):
     return tokenizer(
         examples["text"],
         truncation=True,
-        max_length=512,
+        max_length=128,
         padding="max_length"
     )
 
@@ -35,15 +35,15 @@ training_args = TrainingArguments(
     output_dir="models/generator",
     overwrite_output_dir=True,
     num_train_epochs=3,
-    per_device_train_batch_size=1,
-    gradient_accumulation_steps=4,
+    per_device_train_batch_size=16, # 16 for ThinkPad, 2 for tower
+    gradient_accumulation_steps=1, # 1 for Thinkpad, 4 for tower
     save_steps=500,
     save_total_limit=2,
     logging_steps=100,
     learning_rate=5e-5,
     warmup_steps=100,
     fp16=False,
-    use_mps_device=True,  # True For M1 Mac False for Snapdragon ThinkPad
+    use_mps_device=False,  # True For M1 Mac, False for Snapdragon ThinkPad or AMD/Nvidia Tower
 )
 
 
@@ -56,6 +56,7 @@ trainer = Trainer(
 
 
 trainer.train()
+# trainer.train(resume_from_checkpoint=True)
 
 
 model.save_pretrained("models/generator")
